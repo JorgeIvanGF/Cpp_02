5991be8ad49b42aeb57e8fc5322c9770c3d6aacf
JorgeIvanGF_WSL <jorgeivangf@gmail.com>
2025-06-07 17:25:44 +0200

    CHORE: definition of the Destructir, Getter, and  Setter. Creation of main.cpp file and Makefile

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:23:34 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/05/05 18:44:43 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/06/07 17:12:56 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,26 @@ Fixed& Fixed::operator=(const Fixed& other)
 	if (this != &other)            // avoid self-assignment
 		_value = other._value;     // or other.getRawBits()
 	return *this;
+}
+
+// Destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// METHODS______________________________________________
+
+// Getter
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
+
+// Setter
+void Fixed::setRawBits(int raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:23:42 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/05/05 16:23:43 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/06/07 17:15:55 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Fixed.hpp"
+
+int main()
+{
+	std::cout << YELLOW << "--- Creating a ---" << RESET << std::endl;
+	Fixed a;                  // Default ctor
+
+	std::cout << YELLOW << "\n--- Creating b(a) ---" << RESET << std::endl;
+	Fixed b(a);               // Copy ctor
+
+	std::cout << YELLOW << "\n--- Creating c and doing c = b ---" << RESET << std::endl;
+	Fixed c;
+	c = b;                    // Copy assignment
+
+	std::cout << YELLOW << "\n--- Raw bits after creation ---" << RESET << std::endl;
+	std::cout << "a: " << a.getRawBits() << std::endl;
+	std::cout << "b: " << b.getRawBits() << std::endl;
+	std::cout << "c: " << c.getRawBits() << std::endl;
+
+	std::cout << YELLOW << "\n--- Setting c raw bits to 42 ---" << RESET << std::endl;
+	c.setRawBits(42);
+	std::cout << "c now has raw bits: " << c.getRawBits() << std::endl;
+
+	std::cout << YELLOW << "\n--- Exiting main (destructors will run) ---" << RESET << std::endl;
+	return 0;
+}
